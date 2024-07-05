a43f3d26fad5478269d661c440e470a3459b5032
Eduardo Navarrete <eduardo@upgrade.com.mx>
2024-07-05 13:38:46 -0600

    Home

---
+++ consumers.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <?php include_once("phpAssets/head.php"); ?>
+    <title>Amina</title>
+</head>
+<body>
+    <?php include_once("phpAssets/header.php"); ?>
+    <?php include_once("phpAssets/analytics.php"); ?>
+    <div id="big-container">
+    <section class="cons-sec-1">
+        <div class="content-center">
+            <img src="./_images/consumers.png" alt="Logo Amina" class="cons-logo">
+            <h2>Consumidores</h2>
+            <div class="buttons-container">
+                <button>Sociedad de Abasto</button>
+                <button>Usuario Calificado PM</button>
+                <button>Suministro Básico</button>
+                <button>Usuario Calificado</button>
+            </div>
+        </div>
+    </section>
+    <section class="cons-sec-2">
+        <div class="content-center">
+            <h2>Optimiza el proceso de realizar un PPA</h2>
+            <p>Conecta a través de la plataforma a tus procesos de adquisición de energía: Accede a propuestas, herramientas y análisis necesarios para evitar acuerdos riesgosos y asegurar el mejor PPA - antes que cualquiera.</p>
+        </div>
+    </section>
+    <section class="cons-sec-3">
+        <div class="content-wrapper">
+            <div class="text-content">
+                <h2>Selecciona <span>la mejor propuesta</span></h2>
+                <p>Amina no es simplemente una plataforma; es tu aliado en el proceso de evaluación de PPA.</p>
+                <p>Desde proporcionar información de mercado hasta ofrecer métricas personalizables que se adapten a la posición de tu compañía</p>
+            </div>
+            <div class="image-content">
+                <img src="./_images/consumers-rec2.jpg" alt="Interfaz de evaluación de propuestas">
+            </div>
+        </div>
+    </section>
+    <section class="cons-sec-4">
+        <div class="content-center">
+            <h2>Maximiza el potencial</h2>
+            <div class="features">
+                <div class="feature">
+                    <div class="feature-title">
+                        <h3>Simplificando Conexiones</h3>
+                    </div>
+                    <div class="feature-content">
+                        <p>Comunícate mediante el módulo de Q&As, define tu calendario para el proceso de adquisición de energía y ten la certeza de estar evaluando cada uno de los componentes de las propuestas de los Suministradores Calificados con nuestras herramientas de análisis.</p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">
+                        <h3>Comprende los riesgos</h3>
+                    </div>
+                    <div class="feature-content">
+                        <p>Visualiza y compara las condiciones comerciales de las propuestas de suministro calificado. Evalúa diferentes escenarios ante factores cambiantes que puedan representar riesgos a corto y mediano plazo.</p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">
+                        <h3>Propuestas a la medida</h3>
+                    </div>
+                    <div class="feature-content">
+                        <p>Genera un RFP alineado a las necesidades y objetivos de tu compañía con las plantillas y herramientas que te proporciona la plataforma. Comparte tus consumos e información de forma segura a los Suministradores Calificados en un mismo lugar.</p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">
+                        <h3>Monitorea el rendimiento del PPA</h3>
+                    </div>
+                    <div class="feature-content">
+                        <p>De forma automatizada, la plataforma realizará una evaluación precisa del rendimiento de cada una de las propuestas presentadas por los Suministradores Calificados. Podrás revisar de forma visual y numérica las mismas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="cons-sec-5">
+        <div class="content-wrapper">
+            <div class="box left-box">
+                <h3>Dentro de la plataforma</h3>
+                <ul>
+                    <li>Creación y gestión de RFPs</li>
+                    <li>Evaluación de propuestas de Suministro Calificado</li>
+                    <li>Adquisición de Certificados de Energía Limpia, I-RECs, y Bonos de Carbono</li>
+                    <li>Soluciones en sitio tales como generación distribuida y sistemas de almacenamiento de energía</li>
+                </ul>
+            </div>
+            <div class="box right-box">
+                <h3>Complementa con</h3>
+                <ul>
+                    <li>Consultoría integral en el proceso de adquisición de energía mediante un contrato PPA y soluciones en sitio</li>
+                    <li>Proceso de descarbonización eléctrico de tu empresa: Mide, reduce, compensa</li>
+                </ul>
+                <button class="register-button">Regístrate aquí</button>
+            </div>
+        </div>
+    </section>
+
+
+
+
+</body>
+</html>
---
+++ phpAssets/header.php
@@ -0,0 +1,15 @@
+<header>
+	<img id="logo" src="_images/logo.svg"/> 
+	<menu id="menu_header">
+    	<a href="">Soluciones</a>
+		<a href="">Consumidores</a>
+		<a href="">Participaciones MEM</a>
+		<a href="">En Sitio</a>
+		<a href="">UCPM</a>
+		<a href="">Contacto</a>
+    </menu>
+	<div>
+		<a href="">Ingresar</a>
+		<a id="btn_register" href="">Registrate</a>
+	</div>
+</header>
