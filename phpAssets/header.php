d697ff9e876baa1b6f6c9ff10459c40c1a178dae
Hugo Acosta <93677921+hhacosta@users.noreply.github.com>
2024-08-23 10:26:30 -0600

    cambio a nav

--- phpAssets/header.php
+++ phpAssets/header.php
@@ -1,13 +1,13 @@
 <header>
 	<img id="logo" src="_images/logo.svg"/> 
-	<menu id="menu_header">
+	<nav id="menu_header">
     	<a href="">Soluciones</a>
 		<a href="">Consumidores</a>
 		<a href="">Participaciones MEM</a>
 		<a href="">En Sitio</a>
 		<a href="">UCPM</a>
 		<a href="">Contacto</a>
-    </menu>
+</nav>
 	<div>
 		<a href="">Ingresar</a>
 		<span id="btn_register" class="open-popup-btn" href="">Registrate</span>
